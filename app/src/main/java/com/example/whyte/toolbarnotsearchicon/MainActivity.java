2eed84652e534faeac07e55479a6d7d27ccad1c9
huangqun <WhyteRead@163.com>
2017-06-02 16:40:29 +0800

    添加了注释，修改了布局

--- app/src/main/java/com/example/whyte/toolbarnotsearchicon/MainActivity.java
+++ app/src/main/java/com/example/whyte/toolbarnotsearchicon/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.whyte.toolbarnotsearchicon;
 
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -15,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
     }
 
     @Override
@@ -22,12 +36,14 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_search, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("搜索");
+        //设置搜索图标的显示位置，false为显示编辑框在外面，true为显示在编辑框里面
         searchView.setIconifiedByDefault(false);
         ImageView imageView = (ImageView) searchView.findViewById(R.id.search_mag_icon);
         imageView.setAdjustViewBounds(true);
-        imageView.setMaxWidth(0);
+        imageView.setMaxWidth(0);//设置最大的宽度为0
         imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        imageView.setImageDrawable(null);
+        imageView.setImageDrawable(null);//设置资源文件为null
         return super.onCreateOptionsMenu(menu);
     }
 
